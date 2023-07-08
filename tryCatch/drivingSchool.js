4546c20b4a987b9e5da23824ee3064493537d8ff
Leonardo Grzybowski <leonardogrz@hotmail.com>
2023-07-08 03:37:13 -0300

    feat: drivingSchool.js added

---
+++ tryCatch/drivingSchool.js
@@ -0,0 +1,17 @@
+const checkInfo = (name, age) => {
+  if (!name || !age)
+    throw new Error('Todas as informações são necessárias');
+  if (age < 18)
+    throw new Error('Ops, infelizmente nesse momento você não pode fazer as aulas');
+}
+
+const studentRegister = (name, age) => {
+  try {
+    checkInfo(name, age)
+    return `${name}, seja bem-vindo(a) à AuTrybe!`;
+  } catch (error) {
+    return error.message
+  }
+};
+
+console.log(studentRegister('Leo', 17));
